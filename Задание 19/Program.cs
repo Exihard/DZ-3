7899f85bb2846bf06c0305948f412adbb5fe0314
Korehov_Roman_Nikolaevich <rkorekhov@yandex.ru>
2022-11-26 23:55:48 +0300

    DZ-3 (19, 21, 23)

---
+++ Задание 19/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите любое число");
+var input = Convert.ToInt32(Console.ReadLine());
+var input1 = Convert.ToInt32(input);
+var output1 = Convert.ToInt32(input);
+var output2 = 0;
+var count = 0;
+while (input1 > 0)
+{
+    input1 = input1 / 10;
+    count += 1;
+}
+var i = count;
+var j = Convert.ToInt32(Math.Pow(10, i-1));
+var g = j;
+var step = 1;
+        //Console.Write($"{input % 10}"); 
+        while (step != i)
+        {
+            output1 /= 10;
+            //Console.Write(output1 % 10);
+            step = step + 1;
+            output2 += (output1 % 10)*j;
+            j = j / 10;
+        }
+        //Console.WriteLine("");
+        output2 = (output2 / 10) + ((input % 10)*g);
+if(output2 == input)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Введенное число палиндром!");
+    }
+else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Введенное число не палиндром!");
+    }
+    Console.ResetColor();
---
+++ Задание 23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число до которого вуывести таблицу кубов:");
+var interval = Convert.ToInt32(Console.ReadLine());
+var intermediateNum = Convert.ToInt32(0);
+var i = Convert.ToInt32(1);
+
+while (i <= interval)
+{
+    intermediateNum = Convert.ToInt32(Math.Pow((i),3));
+    Console.WriteLine($"{intermediateNum} ");
+    i = i+1;
+}
